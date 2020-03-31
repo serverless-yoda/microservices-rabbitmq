5bdde6949c0e3d35d688a53f585e2faf21e85168
manny i. agbayani <serverless.yoda@gmail.com>
2020-03-31 14:42:30 +1300

    Infrastructure.IoC

--- RMQ.Domain.Core/Bus/IEventBus.cs
+++ RMQ.Domain.Core/Bus/IEventBus.cs
@@ -6,8 +6,10 @@ namespace RMQ.Domain.Core.Bus
 {
     public interface IEventBus
     {
+        //Command is using Message class w/c used IRequest<bool>
         Task SendCommand<T>(T command) where T : Command;
 
+        //Event only have TimeStamp property
         void Publish<T>(T @event) where T : Event;
 
         void Subscribe<T, TH>() 
---
+++ RMQ.Infrastructure.IoC/DIContainer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RMQ.Domain.Core.Bus;
+using RMQ.Infrastructure.Bus;
+
+namespace RMQ.Infrastructure.IoC
+{
+    public class DIContainer
+    {
+        public static void RegisterServices(IServiceCollection services) {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
